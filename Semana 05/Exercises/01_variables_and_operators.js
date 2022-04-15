17007d94c591dd8e71fd7b3e9d70fa52b7be188f
JuanBSantomero <juanbautista.santomero@gmail.com>
2022-04-15 17:05:39 -0300

    BaSP-2022: ex01-variables and operators

---
+++ Semana 05/Exercises/01_variables_and_operators.js
@@ -0,0 +1,14 @@
+var variableA = 12;
+var variableB = 5;
+var variableC = variableA + variableB
+console.log("ex01-variables and operators-A:", variableC)
+
+var myName = "Bautista"
+var myLastName = "Santomero"
+var fullName = myName + myLastName
+console.log("ex01-variables and operators-B: ", fullName)
+
+var variable1 = "Amazing"
+var variable2 = "Terrible"
+var variable3 = variable1 + variable2
+console.log("ex01-variables and operators-C: ", variable3.length)
