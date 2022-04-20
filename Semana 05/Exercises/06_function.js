e2a1529177dabf4cdfe8b96360526552d9d65ca7
JuanBSantomero <juanbautista.santomero@gmail.com>
2022-04-19 21:46:47 -0300

    BaSP-2022: CORRECTIONS EX06-FUNCTIONS

--- Semana 05/Exercises/06_function.js
+++ Semana 05/Exercises/06_function.js
@@ -3,7 +3,7 @@ function suma(number1, number2){
 }
 var result = suma(15, 18)
 console.log ("ex06-functions-A", result)
-function validate(number1, number2){
+function validateB(number1, number2){
     if (isNaN(number1) || isNaN(number2)){
         console.log("ex06-functions-B", "Error")
         return NaN
@@ -12,62 +12,38 @@ function validate(number1, number2){
         return number1 + number2
     }
 }
-var result = validate(15, 18)
+var result = validateB(15, 18)
 console.log("ex06-functions-B", result)
 function validateInteger(number3){
     return Number.isInteger(number3)
 }
-var solution = validateInteger(5)
-console.log("ex06-functions-C", solution)
-function validate(number1, number2){
+var result = validateInteger (5)
+console.log("ex06-functions-C", result)
+function validateD(number1, number2){
     if(typeof(number1) === "number" && typeof(number2) === "number"){
         if(Number.isInteger(number1) && Number.isInteger(number2)){
             return number1 + number2
         }
         else{
-            alert("Non-integer number")
-            if(!Number.isInteger(number1)){
-                return Math.round(number1)
+           if(!Number.isInteger(number1) && !Number.isInteger(number2)){
+                return Math.round(number1) + Math.round(number2);
             }
-            else{
-            return Math.round(number2)
+            if(!Number.isInteger(number1)){
+                return Math.round(number1) + number2
+            }
+            if(!Number.isInteger(number2)){
+                return Math.round(number2) + number1;
             }
         }
     }
     else{
-        alert("Not a number")
+        console.log("Not a number")
         return NaN
     }
 }
-var result = validate (25, 31.7)
+var result = validateD(25, 26.5)
 console.log("ex06-functions-D", result)
-function validate(number1, number2){
-    if(typeof(number1) === "number" && typeof(number2) === "number"){
-        if(validateInteger(number1, number2)){
-            return number1 + number2
-        }
-        else{
-            alert("Non-integer number")
-            if(!Number.isInteger(number1)){
-                return Math.round(number1)
-            }
-            else{
-            return Math.round(number2)
-            }
-        }
-    }
-    else{
-        alert("Not a number")
-        return NaN
-    }
+function sumaE(number1,number2){
+    return validateD(number1, number2);
 }
-function validateInteger(number1, number2){
-    if(Number.isInteger(number1) && Number.isInteger(number2)){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-var finalsolution = validate(25, 31.7);
-console.log("ex06-functions-E",finalsolution)
+console.log("ex06-functions-E",sumaE(2,38.2))
