0f412ae2d8b9bdb1d66e65425631bfd7c758c8b7
JuanBSantomero <juanbautista.santomero@gmail.com>
2022-04-25 19:27:21 -0300

    BaSP-2022: Login validations correct and advance on sign up validations

--- Semana 06/JavaScript/login.js
+++ Semana 06/JavaScript/login.js
@@ -4,32 +4,52 @@ var password = document.getElementById("password");
 var emailError = document.getElementById("error-email");
 var requiredEmail = document.getElementById("email-required");
 var passwordError = document.getElementById("error-password");
-var requiredPassword = document.getElementById("password-required")
-var login = document.getElementById("input-login")
-form.addEventListener("subtmit", (e) =>{
+var requiredPassword = document.getElementById("password-required");
+var login = document.getElementById("input-login");
+var loginComplete = document.getElementById("login-complete");
+
+form.addEventListener("submit", (e) => {
     e.preventDefault();
+    if(validateEmail() && validatePassword()){
+        loginComplete.classList.add("loginComplete");
+        loginComplete.innerHTML = "Login Complete"+ "<p></p>" + "Email: "+email.value+"<p></p>"+
+        "Password: "+password.value+"</p>";
+    }
+    else if (!validateEmail() || !validatePassword()){
+        loginComplete.classList.add("loginComplete");
+        loginComplete.innerHTML = "Email incorrect or password incorrect"+"<p></p>" + "Email: "+email.value+"<p></p>"+
+        + "Password: "+password.value+"</p>";
+
+    }
 })
+
 email.addEventListener( "blur", validateEmail)
 function validateEmail(e){
     var emailval = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/
     if(email.value === ""){
         requiredEmail.style.display = "flex";
-        requiredEmail.style.justifyContent = "center"
-        email.style.border = "solid 2px red"
+        requiredEmail.style.justifyContent = "center";
+        email.style.border = "solid 2px red";
+        return false
     }
     else{
         if(!emailval.test(email.value)){
-            emailError.style.display = "flex"
-            emailError.style.justifyContent = "center"
-            email.style.border = "solid 2px red"
+            emailError.style.display = "flex";
+            emailError.style.justifyContent = "center";
+            email.style.border = "solid 2px red";
+            return false
+        }
+        else{
+            return true
         }
     }
 }
+
 email.addEventListener("focus", writingEmail)
 function writingEmail(e){
     requiredEmail.style.display = "none";
     emailError.style.display ="none";
-    email.style.border = "solid 2px black"
+    email.style.border = "solid 2px black";
 }
 
 password.addEventListener("blur",validatePassword)
@@ -56,20 +76,22 @@ function validatePassword(e){
         }
     }
     if(password.value === ""){
-        requiredPassword.style.display = "flex"
-        requiredPassword.style.justifyContent = "center"
-        password.style.border = "solid 2px red"
+        requiredPassword.style.display = "flex";
+        requiredPassword.style.justifyContent = "center";
+        password.style.border = "solid 2px red";
+        return false;
     }
-    else if(password.value.length<8 || simbols<1 || numbers<1 || characters<1){
-        passwordError.style.display = "flex"
-        passwordError.style.justifyContent = "center"
-        password.style.border = "solid 2px red"
-        return false
+    else if(password.value.length<8 || simbols>0 || numbers<1 || characters<1){
+        passwordError.style.display = "flex";
+        passwordError.style.justifyContent = "center";
+        password.style.border = "solid 2px red";
+        return false;
     }
     else{
-        return true
+        return true;
     }
 }
+
 password.addEventListener("focus", writingPassword)
 function writingPassword(e){
     requiredPassword.style.display = "none";
---
+++ Semana 06/JavaScript/sign-up.js
@@ -0,0 +1,145 @@
+var form = document.getElementById("form");
+var firstName = document.getElementById("first-name");
+var lastName = document.getElementById("last-name");
+var DNI = document.getElementById("DNI");
+var birthDate = document.getElementById("birth-date");
+var phone = document.getElementById("phone");
+var adress = document.getElementById("adress");
+var city = document.getElementById("city");
+var postCode = document.getElementById("post-code");
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var repPassword = document.getElementById("repeat-password");
+var fNameRequired = document.getElementById("fname-required");
+var fNameInvalid = document.getElementById("invalid-fname");
+var lNameRequired = document.getElementById("lname-required");
+var lNameInvalid = document.getElementById("invalid-lname");
+var DNIrequired = document.getElementById("DNI-required");
+var DNIinvalid = document.getElementById("DNI-invalid");
+var bDateRequired = document.getElementById("BDate-required");
+var bDateInvalid = document.getElementById("BDate-invalid");
+var phoneRequired = document.getElementById("Phone-required");
+var phoneInvalid = document.getElementById("Phone-invalid");
+var adressRequired = document.getElementById("Adress-required");
+var adressInvalid = document.getElementById("Adress-invalid");
+var cityRequired = document.getElementById("City-required");
+var cityInvalid = document.getElementById("City-invalid");
+var pCodeRequired = document.getElementById("PCode-required");
+var pCodeInvalid = document.getElementById("PCode-invalid");
+var requiredEmail = document.getElementById("error-required");
+var emailError = document.getElementById("error-email");
+var requiredPassword = document.getElementById("password-required");
+var passwordError = document.getElementById("error-password");
+var passwordConRequired = document.getElementById("passwordcon-required");
+var passwordNotMatch = document.getElementById("password-doesnt-match")
+var signUp = document.getElementById("input-signup");
+var reset = document.getElementById("input-reset");
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+})
+
+firstName.addEventListener("blur", validateFName)
+function validateFName(e){
+    var Letters = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
+    "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+    "X", "Y", "Z"];
+    var FNCh = 0;
+    var FNSm = 0;
+    for(i=0;i<firstName.value.length;i++){
+        if(!Letters.includes(firstName.value[i])){
+            FNSm++
+        }
+        if(Letters.includes(firstName.value[i])){
+            FNCh++
+        }
+    }
+    if (firstName.value ===""){
+        fNameRequired.style.display = "block";
+        firstName.style.border = "solid 2px red";
+        return false
+    }
+    else if(firstName.value.length<3 || FNCh<1 || FNSm>0){
+        fNameInvalid.style.display ="block";
+        firstName.style.border = "solid 2px red";
+        return false
+    }
+    else{
+        return true
+    }   
+}
+firstName.addEventListener("focus", writtingName)
+function writtingName(e){
+    fNameInvalid.style.display = "none";
+    fNameRequired.style.display = "none";
+    firstName.style.border = "solid 2px black"
+}
+
+lastName.addEventListener("blur", validateLName)
+function validateLName(e){
+    var Letters = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
+    "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+    "X", "Y", "Z"];
+    var LNCh = 0;
+    var LNSm = 0;
+    for(i=0;i<lastName.value.length;i++){
+        if(!Letters.includes(lastName.value[i])){
+            LNSm++
+        }
+        if(Letters.includes(lastName.value[i])){
+            LNCh++
+        }
+    }
+    if (lastName.value ===""){
+        lNameRequired.style.display = "block";
+        lastName.style.border ="solid 2px red";
+        return false;
+    }
+    else if (lastName.value.length<3 || LNCh<1 || LNCh>0){
+        lNameInvalid.style.display = "block";
+        lastName.style.border ="solid 2px red";
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+lastName.addEventListener("focus", writtingLName)
+function writtingLName(e){
+    lNameRequired.style.display = "none";
+    lNameInvalid.style.display = "none";
+    lastName.style.border = "solid 2px black";
+}
+DNI.addEventListener("blur", validateDNI)
+function validateDNI(e){
+    var Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var DNINum = 0;
+    var DNISm = 0;
+    for(i=0;i<DNI.value.length;i++){
+        if(!Numbers.includes(DNI.value[i])){
+            DNISm++
+        }
+        if(Numbers.includes(DNI.value[i])){
+            DNINum++
+        }
+    }
+    if(DNI.value === ""){
+        DNIrequired.style.display = "block";
+        DNI.style.border = "solid 2px red";
+    }
+    else if(DNI.value.length<7 || DNINum<1 || DNISm>0){
+        DNIinvalid.style.display = "block";
+        DNI.style.border = "solid 2px red";
+    }
+}
+DNI.addEventListener("focus", writtingDNI)
+function writtingDNI(e){
+    DNIrequired.style.display = "none";
+    DNIinvalid.style.display = "none";
+    DNI.style.border = "solid 2px black";
+}
+
